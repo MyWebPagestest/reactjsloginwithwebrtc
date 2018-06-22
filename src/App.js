ef93ad0352a3fb37b779d66741a11b6d849b1693
RT1919 <40264790+RT1919@users.noreply.github.com>
2018-06-22 12:16:43 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import { Navbar, Button } from 'react-bootstrap';
+import './App.css';
+
+class App extends Component {
+  goTo(route) {
+    this.props.history.replace(`/${route}`)
+  }
+
+  login() {
+    this.props.auth.login();
+  }
+
+  logout() {
+    this.props.auth.logout();
+  }
+
+  render() {
+    const { isAuthenticated } = this.props.auth;
+
+    return (
+      <div className='navbar'>
+        <Navbar color="blue-grey lighten-5">
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#">CamToy</a>
+
+            </Navbar.Brand>
+            <Button
+              bsStyle="primary"
+              className="btn-margin"
+              color={'red'}
+              onClick={this.goTo.bind(this, 'home')}
+              
+            >
+              Home
+            </Button>
+            {
+              !isAuthenticated() && (
+                  <Button
+                    id="qsLoginBtn"
+                    bsStyle="primary"
+                    className="btn-margin"
+                    color="#841584"
+                    onClick={this.login.bind(this)}
+                  >
+                    Log In
+                  </Button>
+                )
+            }
+            {
+              isAuthenticated() && (
+                  <Button
+                    id="qsLogoutBtn"
+                    bsStyle="primary"
+                    className="btn-margin"
+                    color="#843484"
+                    onClick={this.logout.bind(this)}
+                  >
+                    Log Out
+                  </Button>
+                )
+            }
+          </Navbar.Header>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default App;
