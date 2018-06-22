2037b2d1e635b3fc004bf9d11989e0adea779c18
RT1919 <40264790+RT1919@users.noreply.github.com>
2018-06-22 14:07:50 +0200

    Update auth-variables.js

--- src/auth/auth-variables.js
+++ src/auth/auth-variables.js
@@ -1,5 +1,6 @@
 export const AUTH_CONFIG = {
-  domain: 'camt.eu.auth0.com',
-  clientId: 'aesa7bdhFETdw87yjxiHfiKtdcp3rtHR',
-  callbackUrl: 'http://localhost:3000/callback'
+  domain: 'YOUR AUTH0 DOMAIN NAME',
+  clientId: 'YOUR CLIENT ID',
+  callbackUrl: 'http://localhost:3000/callback' //this depends what callback address you want to give.
+                                                //if you change it dont forget to change your callback.js file too
 }
