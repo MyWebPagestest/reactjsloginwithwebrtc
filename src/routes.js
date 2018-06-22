e6d386e78852e846dc307914bdf5dc04b68f5850
RT1919 <40264790+RT1919@users.noreply.github.com>
2018-06-22 12:20:37 +0200

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Router } from 'react-router-dom';
 import App from './App';
 import Home from './Home/Home';
-import Callback from './Callback/Callback';
+import Callback from './Callback';
 import Auth from './Auth/Auth';
 import history from './history';
 
