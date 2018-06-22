2484eb40dee9fd43bd3637336f85da349d349f8a
RT1919 <40264790+RT1919@users.noreply.github.com>
2018-06-22 15:28:01 +0200

    Update auth-variables.js

--- src/auth/auth-variables.js
+++ src/auth/auth-variables.js
@@ -1,6 +1,5 @@
 export const AUTH_CONFIG = {
-  domain: 'YOUR AUTH0 DOMAIN NAME',
-  clientId: 'YOUR CLIENT ID',
-  callbackUrl: 'http://localhost:3000/callback' //this depends what callback address you want to give.
-                                                //if you change it dont forget to change your callback.js file too
+  domain: '',
+  clientId: '',
+  callbackUrl: 'http://localhost:3000/callback'
 }
